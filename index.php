bf3ed4c451f7cd022bbddcc5376693ac6171b05d
nicholasfay <nicholas.fay5@gmail.com>
2019-08-13 19:12:13 -0400

    Updated styles

--- index.php
+++ index.php
@@ -18,7 +18,6 @@
         </div>
         <hr class="text-secondary w-75" />
         <h1 class="center w-75 mt-5 ml-auto mr-auto">Or some of these great products below</h1>
-        <hr class="text-secondary w-25 center" />
         <div class="native-ads" id="amzn-assoc-ad-fd273be0-cd17-4543-b9cf-c8c304c8367e"></div>
         <hr class="text-secondary w-75" />
     </section>
