409913d24eec9501e48ec5425d1322b119b07dd0
nicholasfay <nicholas.fay5@gmail.com>
2021-05-27 22:11:40 -0400

    Put corey on adults avail page

--- adultsavail.php
+++ adultsavail.php
@@ -4,23 +4,23 @@
 <?php includeWithVariables("head.php", array('title' => 'Adults Available - Skyline Giant Schnauzers'))?>
 
 <body>
-
     <!-- Header -->
     <?php includeWithVariables("header.php", array('headerClass' => 'adults-avail-header', 'headerHTML' => '<h1>Adults Available</h1>'));?>
 
     <!-- Page Content -->
     <section class="content-section-a bg-color">
         <div class="container">
+            <div class="row justify-content-center descript"><img class="img-fluid lazy"
+                    sizes="(max-width: 600px) 100vw, 600px" data-srcset="
+                        https://res.cloudinary.com/djjyxjoz2/image/upload/c_scale,w_600/IMG_6117_qizouo.heic 600w"
+                    data-src="https://res.cloudinary.com/djjyxjoz2/image/upload/c_scale,w_600/IMG_6117_qizouo.heic"
+                    alt="Giant Schnauzer puppy cropped for sale" title="Giant Schnauzer puppy cropped for sale"></div>
             <div class="row justify-content-center descript">
                 Young adult male available to the right home. Looking for a one or two dog home (other dog must be a
                 spayed female).
-
-            </div>
-            <div class="row justify-content-center descript">
                 Requirements: an experienced Giant or working dog owner who has ample time to spend training with him,
                 and who can provide him a consistent structured daily routine, that includes work, fun, and lots of love
-                and attention. Must be on the east coast and in close proximity to Long Island. He will be required to
-                remain in my breeding program.
+                and attention.
             </div>
 
             <!-- <div class="descript">Adults are currently available! Go to our <a
